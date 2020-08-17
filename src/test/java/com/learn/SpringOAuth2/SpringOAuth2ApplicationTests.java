fbe553f53c5299b6a2a520296175256614abedbc
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-08-17 23:02:15 +0530

    Initial commit

---
+++ src/test/java/com/learn/SpringOAuth2/SpringOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.SpringOAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
